e100457c3b6817fe8754741830c41b1bd0502279
odina <odinasayid@gmail.com>
2023-10-26 12:32:39 -0700

    POST, DELETE, GET API endpoint added, connected to MONGO database, created mongo layer mongo.js files

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,3 +1,7 @@
+const launchesDatabase = require('./launches.mongo');
+const planets = require('./planets.mongo');
+
+const DEFAULT_FLIGHT_NUMBER = 100;
 const launches = new Map();
 
 let latestFlightNumber = 100;
@@ -12,38 +16,73 @@ const launch = {
     upcoming: true,
     success: true,
 }
+saveLaunch(launch);
+// launches.set(launch.flightNumber, launch);
 
-launches.set(launch.flightNumber, launch);
-
-function existsLaunchWithId(launchId) {
-    return launches.has(launchId);
+async function existsLaunchWithId(launchId) {
+    return await launchesDatabase.findOne({
+        flightNumber: launchId,
+    });
 }
-function getAllLaunches() {
-    return Array.from(launches.values());
-}
+// to solve auto increment in MongoDB
+// sort in descending order(-) and get the 
+async function getLatestFlightNumber() {
+    const latestLaunch = await launchesDatabase.findOne().sort('-flightNumber');
 
-function addNewLaunch(launch){
-    latestFlightNumber++;
-    launches.set( latestFlightNumber,
-        Object.assign(launch, {
-            success: true,
-            upcoming: true,
-            customers: ['Zero to Mastery', 'NASA'],
-            flightNumber: latestFlightNumber,
-        })
-    );
+    if(!latestLaunch){
+        return DEFAULT_FLIGHT_NUMBER;
+    }
+    return latestLaunch.flightNumber;
 }
+//second argument for excluding mongoose key arguments
+async function getAllLaunches() {
+    return await launchesDatabase.find({},{
+        '_id':0,'__v':0,
+    });
+}
+// the parameter that updateOne takes is the parameter that we are looking for,
+// if exists save it to launches, otherwise update that object information
+async function saveLaunch(launch){
+    const planet = await planets.findOne({
+        keplerName: launch.target,
+    });
 
-function abortLaunchById(launchId){
-    const aborted = launches.get(launchId);
-    aborted.upcoming = false;
-    aborted.success = false;
-    return aborted;
+    if(!planet) {
+        throw new Error('No matching plalnet found');
+    }
+
+    await launchesDatabase.findOneAndUpdate({
+        flightNumber: launch.flightNumber,
+    }, launch, {
+        upsert: true,
+    });
+}
+async function scheduleNewLaunch(launch){
+    const newFlightNumber = await getLatestFlightNumber() + 1;
+    const newLaunch = Object.assign(launch, {
+        success: true,
+        upcoming: true,
+        customers: ['Zero to Mastery', 'NASA'],
+        flightNumber: newFlightNumber,
+    });
+
+    await saveLaunch(newLaunch);
+}
+async function abortLaunchById(launchId){
+    //find the launch and update list of
+    //properties of it 
+   const aborted = await launchesDatabase.updateOne({
+        flightNumber: launchId,
+    }, {
+        upcoming: false,
+        success: false,
+    })
+    return aborted.modifiedCount===1;
 }
 
 module.exports = {
     existsLaunchWithId,
     getAllLaunches,
-    addNewLaunch,
     abortLaunchById,
+    scheduleNewLaunch, 
 }
--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const path = require('path');
 const {parse} = require('csv-parse');
 
+const planets = require('./planets.mongo');
+
 function isHabitablePlanet(planet){
     return planet['koi_disposition']==='CONFIRMED' && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
     && planet['koi_prad'] < 1.6;
@@ -16,7 +18,7 @@ function loadPlanetsData() {
 }))
 .on('data', async (data) => {
     if(isHabitablePlanet(data)){
-        habitablePlanets.push(data);
+        savePlanet(data);
     }
 }).on('error',(err) => {
     console.log(err);
@@ -31,8 +33,24 @@ function loadPlanetsData() {
     
 }
 
-function getAllPlanets() {
-    return habitablePlanets;
+async function getAllPlanets() {
+    return await planets.find({},{
+        '_id': 0, '__v':0,
+    });
+}
+async function savePlanet(planet) {
+    try {
+        await planets.updateOne({
+            keplerName: planet.kepler_name,
+        },{
+            keplerName: planet.kepler_name,
+        }, {
+            upsert: true,
+        });
+    } catch(err) {
+        console.error(`Could not save planet ${err}`)
+    }
+    
 }
 
 module.exports = {
